6f1af176b0ca37413fed0db8134c8ac369be73ce
Przemysław Gołąb (SirPigeonz) <golab.przemyslaw@gmail.com>
2023-02-15 16:10:56 +0100

    Add ability for a Bloom layer to use Gaussian Blur filter or Simple Blur

--- scripts/bloom_layer.py
+++ scripts/bloom_layer.py
@@ -5,7 +5,8 @@ D = K.activeDocument()
 N = D.activeNode()
 
 
-def create_bloom_layer():
+def create_bloom_layer(gauss_blur: bool = True):
+    #print(K.filters())
     parent = N.parentNode()
     
     if D.selection() == None:
@@ -20,34 +21,23 @@ def create_bloom_layer():
     else:
         selection = D.selection()
     
-    
     curves = K.filter("perchannel")
     bloom_layer = D.createFilterLayer("Bloom Layer", curves, selection)
-    blur = K.filter("blur")
-    blur_mask = D.createFilterMask("Blur", blur, selection)
-    #transparency_mask = D.createNode("Transparency", "transparencymask")
-    #s = selection
-    #transparency_mask.setPixelData(selection.pixelData(s.x(), s.y(), s.width(), s.height()), s.x(), s.y(), s.width(), s.height())
-    #print(transparency_mask.channels())
-    #bloom_layer.addChildNode(transparency_mask, None)
+    
+    n_name = ""
+    blur = None
+    if gauss_blur:
+        blur = K.filter("gaussian blur")
+        n_name = "Gaussian Blur"
+    else:
+        blur = K.filter("blur")
+        n_name = "Simple Blur"
+    
+    blur_mask = D.createFilterMask(n_name, blur, selection)
     bloom_layer.addChildNode(blur_mask, None)
     bloom_layer.setBlendingMode("screen")
     parent.addChildNode(bloom_layer, N)
-    
-    
-    '''
-    #group = D.createGroupLayer("Bloom")
-    clone = D.createCloneLayer("Bloom", N)
-    print(K.filters())
-    curve_filter = K.filter("perchannel")
-    curve_fmask = D.createFilterMask("Curve Filter", curve_filter, selection)
 
-    #parent.addChildNode(group, N)
-    #group.addChildNode(clone, None)
-    clone.addChildNode(curve_fmask, None)
-    parent.addChildNode(clone, N)
-    '''
-    
 
-create_bloom_layer()
+create_bloom_layer(True)
 
